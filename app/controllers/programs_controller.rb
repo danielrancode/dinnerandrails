ac760f58f60eb837da9c42d6e75c4efda5bd6cfd
Daniel Ran <danielr1735@gmail.com>
2018-07-24 14:58:18 -0400

    added some functionality and views to programs

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -1,2 +1,45 @@
 class ProgramsController < ApplicationController
+
+  def index
+    # byebug
+    @user = User.find(params[:user_id])
+    @programs = @user.programs
+  end
+
+  def show
+    @user = User.find(params[:user_id])
+    @program = @user.programs.find(params[:id])
+  end
+
+  def new
+    @user = User.find(params[:user_id])
+    @program = Program.new(user: @user)
+  end
+
+  def create
+    @user = User.create(user_params)
+    redirect_to user_path(@user)
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to users_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
-  get 'user/index'
-  get 'user/show'
-  get 'user/new'
-  get 'user/create'
-  get 'user/edit'
-  get 'user/update'
-  get 'user/destroy'
   resources :restaurants
   resources :events
-  resources :programs
-  resources :users
+
+  resources :users do
+    resources :programs
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
